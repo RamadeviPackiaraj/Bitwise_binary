166d4859741d7e8e17fb73e47624f1df938fea9f
RamadeviPackiaraj <149371902+RamadeviPackiaraj@users.noreply.github.com>
2025-08-09 13:42:04 +0530

    Add files via upload

---
+++ Integer_to_binary.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Integer_to_binary{
+    public static String Convert_Binary(int n){
+       StringBuilder sb=new StringBuilder();
+        while(n!=1){
+            if(n%2==1) sb.append('1');
+            else sb.append('0');
+            n/=2;
+        }
+        return sb.reverse().toString();
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(Convert_Binary(n));
+    }
+}
